5e7327f52df4202e6a5015a28d9d9a2ed256a102
HS6103 <hsuchen1022@gmail.com>
2024-05-15 10:14:09 +0800

    fixed bug in app.py

--- app.py
+++ app.py
@@ -50,14 +50,13 @@ def linebot():
             reply = resultDICT["response"][0]   #回傳回覆字串
         else:
             reply = '你傳的不是文字呦～請再試一次'
-            
+        print(reply)
+        line_bot_api.reply_message(tk,TextSendMessage(reply))    # 回傳訊息
+
     except:
         print(body)                                                                   # 如果發生錯誤，印出收到的內容
-        reply = '我是預設的回覆字串，目前機器人發生了一些問題，請稍後再試一次!'                 # 如果發生錯誤，預設的回覆訊息
+        reply = '我是預設的回覆字串，目前機器人發生了一些問題，請稍後再試一次!'            # 如果發生錯誤，預設的回覆訊息
                 
-    print(reply)
-    line_bot_api.reply_message(tk,TextSendMessage(reply))    # 回傳訊息
-
     return 'OK'                                              # 驗證 Webhook 使用，不能省略
 
 if __name__ == "__main__":
