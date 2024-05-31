6c10a3e17d7e00a8e782610f1f7eb07076afadfd
HS6103 <hsuchen1022@gmail.com>
2024-05-31 14:58:01 +0800

    added utterance in classes

--- starvoice/intent/Loki_classes.py
+++ starvoice/intent/Loki_classes.py
@@ -138,5 +138,31 @@ def getResult(inputSTR, utterance, args, resultDICT, refDICT, pattern=""):
                 resultDICT["response"] = "抱歉，我們沒有{}社課喔!".format(args[5])
         else:
             pass
+        
+    if utterance == "有沒有社課":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+        else:
+            if CHATBOT_MODE:
+                if args[3] == None:
+                    resultDICT["response"] = getResponse("有社課嗎", args)
+                else:
+                    args[3] = args[3].replace(" ", "").lower()
+                    if args[3] in ["鼓", "爵士鼓"]:
+                        resultDICT["response"] = getResponse("鼓社課是什麼時候", args)
+                    elif args[3] in ["pa"]:
+                        resultDICT["response"] = getResponse("PA社課是什麼時候", args)                
+                    elif args[3] in ["貝斯", "bass"]:
+                        resultDICT["response"] = getResponse("貝斯社課是什麼時候", args)
+                    elif args[3] in ["kb", "keyboard"]:
+                        resultDICT["response"] = getResponse("KB社課是什麼時候", args)
+                    elif args[3] in ["主唱", "vocal"]:
+                        resultDICT["response"] = getResponse("主唱社課是什麼時候", args)
+                    elif args[3] in ["吉他", "結他"]:
+                        resultDICT["response"] = getResponse("吉他社課是什麼時候", args)
+                    else:
+                        resultDICT["response"] = "抱歉，我們沒有{}社課喔!".format(args[3])
+            else:
+                pass
 
     return resultDICT
